d047cd85f7d4678f02160314feac9848a5e4ac37
晶 黄 <huangjing@oneniceapp.com>
2020-11-21 19:57:15 +0800

    fix

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+	  <div>test01000</div>
+	<!-- <transition>
+		<keep-alive>
+		  <router-view v-if="this.$router.currentRoute.meta.keepAlive" />
+		</keep-alive>
+	</transition>
+	<transition>
+		<router-view v-if="!this.$router.currentRoute.meta.keepAlive" />
+	</transition> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
